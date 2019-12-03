0cdb885ac6387054fb3437ad99ae576f030f500f
noayair <noashefer5@gmail.com>
2019-12-03 20:47:54 +0200

    updateInitFromString

--- src/complexFunction.java
+++ src/complexFunction.java
@@ -1,50 +1,308 @@
-public class complexFunction {
-    function left , right;
-    Operation op;
+public class complexFunction implements function {
+    private Node root;
+    private Operation op;
+    //private Node parent;
 
-    complexFunction(function left , function right ,  Operation op){
-       this.left = left;
-       this.right = right;
-       this.op = op;
+    public complexFunction() {
+    this.root = new Node(null , Operation.None , null , null , null);
+    this.root.setLeft(null);
+    this.root.setRight(null);
+    }
+    public complexFunction(function f){
+        Node left = new Node(f , null , null , null , null);
+        this.root = new Node(null , Operation.None , left , null , null);
+        left.setParent(this.root);
+
     }
 
-    public double f(double x){
+    public complexFunction(function f1 , function f2 , String op){
+        switch (op){
+            case "plus":
+                this.root = new Node(null , Operation.Plus , null , null , null);
+                break;
+            case "div":
+                this.root = new Node(null , Operation.Divid , null , null , null);
+                break;
+            case "max":
+                this.root = new Node(null , Operation.Max , null , null , null);
+                break;
+            case "min":
+                this.root = new Node(null , Operation.Min , null , null , null);
+                break;
+            case "comp":
+                this.root = new Node(null , Operation.Comp , null , null , null);
+                break;
+            case "mul":
+                this.root = new Node(null , Operation.Times , null , null , null);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + op);
+        }
+        Node left = new Node(f1 , null ,  null , null , this.root);
+        Node right = new Node(f2 , null , null , null , this.root);
+        this.root.setLeft(left);
+        this.root.setRight(right);
+        //left.setParent(this.root);
+        //right.setParent(this.root);
+    }
+
+    public complexFunction(String s){
+        int i = s.indexOf('(');
+        if (i == -1){
+            Polynom p = new Polynom(s);
+            new complexFunction(p);
+        }
+        //s = s.substring(i+1);
+        String s2 = "";
+        int j = 0;
+        int counter = 0;
+        while(s.charAt(j) == 'p' || s.charAt(j) == 'd' || s.charAt(j)==  'm' || s.charAt(j) == 'c' || s.charAt(j) == 'n') {
+            s2 = s.substring(j, i);
+            switch (s2) {
+                case "plus":
+                    this.op = Operation.Plus;
+                    break;
+                case "div":
+                    this.op = Operation.Divid;
+                    break;
+                case "max":
+                    this.op = Operation.Max;
+                    break;
+                case "min":
+                    this.op = Operation.Min;
+                    break;
+                case "comp":
+                    this.op = Operation.Comp;
+                    break;
+                case "mul":
+                    this.op = Operation.Times;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + op);
+            }
+            if (this.root == null) {
+                Node n = new Node(null , op , null , null , null);
+                this.root = n;
+                counter ++;
+            } else {
+                Node n = new Node(null , op , null , null , this.root);
+                this.root.setLeft(n);
+                this.root = this.root.getLeft();
+                counter++;
+            }
+            s = s.substring(i + 1);
+            i = s.indexOf('(');
+        }
+        i = s.indexOf(',');
+        String s3 = s.substring(0, i);
+        Polynom f1 = new Polynom(s3);
+        Node n = new Node(f1 , null , null , null , null);
+        this.root.setLeft(n);
+        s = s.substring(i+1);
+        while(!s.equals("")) {
+            i = s.indexOf(')');
+            s3 = s.substring(0, i);
+            Polynom f2 = new Polynom(s3);
+            if(this.root.getRight() != null){
+                this.root = this.root.getParent();
+                Node r = new Node(f2 , null , null , null ,this.root);
+                this.root.setRight(r);
+            }else {
+                Node r1 = new Node(f2 , null , null , null , this.root);
+                this.root.setRight(r1);
+            }
+            int t = s.indexOf(',');
+            if(t == -1){
+                s = "";
+            }
+            s = s.substring(t+1);
+        }
+    }
+
+    public double f(double x) {
+        if (this.root.getF() != null) {
+            return this.root.getF().f(x);
+        }
+        switch (this.root.getOp()) {
+            case Plus:
+                return ((this.root.getLeft().getF().f(x)) + (this.root.getRight().getF().f(x)));
+            case Divid:
+                return ((this.root.getLeft().getF().f(x)) / (this.root.getRight().getF().f(x)));
+            case Max:
+                return Math.max((this.root.getLeft().getF().f(x)), (this.root.getRight().getF().f(x)));
+            case Min:
+                return Math.min((this.root.getLeft().getF().f(x)), (this.root.getRight().getF().f(x)));
+            case Comp:
+                return this.root.getLeft().getF().f(this.root.getRight().getF().f(x));
+            case Times:
+                return ((this.root.getLeft().getF().f(x)) * (this.root.getRight().getF().f(x)));
+            case None:
+                return this.root.getLeft().getF().f(x);
+            case Error:
+                return 0;
+        }
         return 0;
     }
 
-    public String toString(){
-        return null;
+    public String print(Node root , String s){
+        s += this.root.getOp();
+        s +="(";
+        //System.out.print(this.root.getOp() + "(");
+        if(this.root.getLeft().getOp() == null){
+            String s1 = print_function(this.root , s);
+            s =s1;
+        }else {
+           s += print(this.root.getLeft(), s);
+           this.root = this.root.getParent();
+            if (this.root.getRight().getF() != null) {
+                s += ",";
+                s += this.root.getRight().getF();
+                s += ")";
+                //System.out.println(this.root.getRight().getF());
+            }
+        }
+        return s;
     }
 
-    public String chekEnum(){
-        switch(this.op){
-            case Plus:
-                return "plus";
-            case Divid:
-                return "divid";
-            case Max:
-                return "max";
-            case Min:
-                return "min";
-            case Comp:
-                return "comp";
-            case Times:
-                return "times";
-            case None:
-                return "none";
-            case Error:
-                return "error";
-        }
-        return null;
+    public String print_function(Node root , String s){
+        s += this.root.getLeft().getF();
+        s += ",";
+        s += this.root.getRight().getF();
+        s += ")";
+        //System.out.print(this.root.getLeft().getF() + ",");
+        //System.out.println(this.root.getRight().getF() + ")");
+        return  s;
     }
+
+    public String toString(){
+        String ans = "";
+       ans += print(this.root , ans);
+       // System.out.println(ans);
+        return ans;
+    }
+
+//    public String chekEnum(){
+//        switch(this.op){
+//            case Plus:
+//                return "plus";
+//            case Divid:
+//                return "divid";
+//            case Max:
+//                return "max";
+//            case Min:
+//                return "min";
+//            case Comp:
+//                return "comp";
+//            case Times:
+//                return "times";
+//            case None:
+//                return "none";
+//            case Error:
+//                return "error";
+//        }
+//        return null;
+//    }
+
+//    public void x(String s){
+//        int i = s.indexOf('(');
+//        if (i == -1){
+//            Polynom p = new Polynom(s);
+//            new complexFunction(p);
+//        }
+//        //s = s.substring(i+1);
+//        String s2 = "";
+//        int j = 0;
+//        int counter = 0;
+//        while(s.charAt(j) == 'p' && s.charAt(j) == 'd' && s.charAt(j)==  'm' && s.charAt(j) == 'c' && s.charAt(j) == 'n') {
+//            s2 = s.substring(j, i);
+//            switch (s2) {
+//                case "plus":
+//                    this.op = Operation.Plus;
+//                    break;
+//                case "div":
+//                    this.op = Operation.Divid;
+//                    break;
+//                case "max":
+//                    this.op = Operation.Max;
+//                    break;
+//                case "min":
+//                    this.op = Operation.Min;
+//                    break;
+//                case "comp":
+//                    this.op = Operation.Comp;
+//                    break;
+//                case "mul":
+//                    this.op = Operation.Times;
+//                    break;
+//                default:
+//                    throw new IllegalStateException("Unexpected value: " + op);
+//            }
+//            if (this.root == null) {
+//                this.root.setOp(op);
+//                counter ++;
+//            } else {
+//                this.root.getLeft().setOp(op);
+//                this.root = this.root.getLeft();
+//                counter++;
+//            }
+//            s = s.substring(i + 1);
+//            i = s.indexOf('(');
+//        }
+//        i = s.indexOf(',');
+//        String s3 = s.substring(0, i);
+//        Polynom f1 = new Polynom(s3);
+//        this.root.getLeft().setF(f1);
+//        s = s.substring(i+1);
+//        while(!s.equals("")) {
+//            i = s.indexOf(')');
+//            s3 = s.substring(0, i);
+//            Polynom f2 = new Polynom(s3);
+//            if(this.root.getRight().getF() != null){
+//                this.root = this.root.getParent();
+//                this.root.getRight().setF(f2);
+//            }else {
+//                this.root.getRight().setF(f2);
+//            }
+//            int t = s.indexOf(',');
+//            if(t == -1){
+//                s = "";
+//            }
+//            s = s.substring(t+1);
+//        }
+//    }
 
     public function initFromString(String s){
-        int t = s.indexOf('(');
-        String s1 = s.substring(0 , t);
-        if(chekEnum() == s1) {
-            this.op = Operation.valueOf(s1);
-        }
-        return null;
+        complexFunction c = new complexFunction(s);
+        return c;
+
+//        int i = s.indexOf('(');
+//        if (i == -1){
+//            Polynom p = new Polynom(s);
+//            return p;
+//        }
+//        String s1 = s.substring(i+1);
+//        String s2 = s.substring(0 , i);
+//        if(s1.charAt(0) != 'p' && s1.charAt(0) != 'd' && s1.charAt(0) !=  'm' && s1.charAt(0) != 'c' && s1.charAt(0) != 'n'){
+//            int foo = s1.indexOf(',');
+//            String s3 = s1.substring(0 , foo);
+//            Polynom f1 = new Polynom(s3);
+//            int foo2 = s1.indexOf(')');
+//            String s4 = s1.substring(foo+1 , foo2);
+//            Polynom f2 = new Polynom(s4);
+//           complexFunction c = new complexFunction(f1 , f2 , s2);
+//            return c;
+//        }
+//       return initFromString(s1);
+//        String arr[] = new String[];
+//        int i = s.indexOf('(');
+//        arr[0] = s.substring(0 , i);
+//        int j = 1;
+//        while (s.charAt(i+1) == 'p' || s.charAt(i+1) == 'd' || s.charAt(i+1) ==  'm' || s.charAt(i+1) == 'c' || s.charAt(i+1) == 'n'){
+//            String s1 = s.substring(i+1);
+//            int t = s1.indexOf('(');
+//            arr[j] = s1.substring(i+1 , t);
+//            j++; i++;
+//        }
     }
     public function copy() {
         return null;
@@ -54,10 +312,9 @@ public class complexFunction {
     }
 
     public void plus(function f1){
-        complexFunction c = new complexFunction(this.left , this.right , this.op);
-     //   complexFunction c1 = new complexFunction(c , f1 , Operation.Plus);
+//        complexFunction c = new complexFunction(this.left , this.right , this.op);
+//     //   complexFunction c1 = new complexFunction(c , f1 , Operation.Plus);
     }
-
     public void mul(function f1){
     }
 
@@ -83,5 +340,31 @@ public class complexFunction {
 
     public Operation getOp(){
         return null;
+    }
+
+   public static void main(String[] args) {
+        Polynom p = new Polynom("x");
+        Polynom p1 = new Polynom("2x");
+        Polynom p2 = new Polynom("3x");
+//        complexFunction c = new complexFunction(p,p1,"plus");
+//       System.out.println(c.f(2));
+       //String s = "plus(plus(3x,3x),3x)";
+      // function c = null;
+      // c = new complexFunction;
+       //System.out.println(c.initFromString(s));
+//       complexFunction c = new complexFunction(p,p1,"plus");
+//       complexFunction c1 = new complexFunction(c,p2,"plus");
+//       System.out.println(c1.toString());
+       //System.out.println(c.toString());
+
+
+//       complexFunction x = new complexFunction();
+//       function y = new complexFunction();
+//       y = x.initFromString("div(plus(x,2x),3x)");
+       //complexFunction y = null;
+       //y = x.initFromString("plus(x,2x)");
+//       String s = "div(plus(x,2x),3x)";
+//       function y = new complexFunction(s);
+//       System.out.println(y.toString());
     }
 }
